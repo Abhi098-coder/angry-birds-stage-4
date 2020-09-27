c5485ccec46654aa0d8e513a33cce29501975ebc
Abhi098-coder <aswal.abhishek.singh@gmail.com>
2020-09-26 20:55:27 -0500

    Made it have a real slingshot

--- Slingshot.js
+++ Slingshot.js
@@ -1,5 +1,8 @@
 class SlingShot{
     constructor(bodyA, pointB){
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         var options = {
             bodyA: bodyA,
             pointB: pointB,
@@ -19,9 +22,22 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(this.sling.bodyA.position.x <= 220){
+                strokeWeight(6);
+                stroke(48,22,8);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                image(this.sling3,pointA.x-30,pointA.y-15, 15, 30);
+            }else{
+                strokeWeight(4);
+                stroke(48,22,8);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                image(this.sling3,pointA.x+25,pointA.y-15, 15, 30);
+            }
         }
+        image(this.sling1,200,30);
+        image(this.sling2,170,30);
     }
     
 }
